bde20bd99b7f0b8ec43494615b7b73012891dfcc
jnoony <tarot0312@nate.com>
2020-07-06 01:36:25 +0900

    Updates

---
+++ precache-manifest.5ccdab84eb0aa8123115f9020dff53db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c139d51d762c435de0e3f88fbcc8da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46c43d2e844071b7f8e7",
+    "url": "/static/js/2.a5d22535.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5d22535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa7c0d5b16ed482fce4",
+    "url": "/static/js/main.3431f694.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
