0b58667fb42b83c0910beea21a977baf27faa70f
jnoony <tarot0312@nate.com>
2020-07-10 17:54:48 +0900

    test

--- src/client/Root.js
+++ src/client/Root.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from '../App'
 
 const Root = () => (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
     </BrowserRouter>
 );
